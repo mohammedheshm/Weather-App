6c312b88cfb3f51d1966787b252e89cd34387081
Mohammedhesham <mohammedhe4am@gmail.com>
2024-09-15 23:29:49 +0300

    Handle UI And logic Of Splash Screen

--- app/src/main/java/com/example/weatherapp/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/weatherapp/splash/SplashScreenActivity.kt
@@ -1,21 +1,56 @@
 package com.example.weatherapp.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.MainActivity
 import com.example.weatherapp.R
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var getStart_btn: Button
+    lateinit var sp_imageView: ImageView
+    lateinit var sp_weatherTv: TextView
+    lateinit var sp_forecastTV: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        sp_imageView = findViewById(R.id.sp_img)
+        sp_weatherTv = findViewById(R.id.sp_weatherTV)
+        sp_forecastTV = findViewById(R.id.sp_forecastTV)
+        getStart_btn = findViewById(R.id.sp_getStart_btn)
+
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val scaleUp = AnimationUtils.loadAnimation(this, R.anim.scale_up)
+        val translate = AnimationUtils.loadAnimation(this, R.anim.translate)
+
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(scaleUp)
+        animationSet.addAnimation(translate)
+        animationSet.duration = 1500
+
+        sp_imageView.startAnimation(animationSet)
+        sp_weatherTv.startAnimation(fadeIn)
+        sp_forecastTV.startAnimation(fadeIn)
+        getStart_btn.startAnimation(fadeIn)
+
+        getStart_btn.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
