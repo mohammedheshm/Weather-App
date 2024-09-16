debffe0feeaf737d6e64d135ee57acf6b32e8ba8
Mohammedhesham <mohammedhe4am@gmail.com>
2024-09-16 23:06:04 +0300

    Handle Logic Of Navigate Between Fragment with navController

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -7,11 +7,50 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+    lateinit var fragmentContainerView: FragmentContainerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fragmentContainerView = findViewById(R.id.nav_host_fragment)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
+
+        val navOptions = NavOptions.Builder()
+            .setRestoreState(true)
+            .setPopUpTo(R.id.home, false, true)
+            .build()
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> navController.navigate(R.id.home_graph, null, navOptions)
+                R.id.alert -> navController.navigate(R.id.alert_graph, null, navOptions)
+                R.id.favorite -> navController.navigate(R.id.favorite_graph, null, navOptions)
+                R.id.settings -> navController.navigate(
+                    R.id.preferences_graph,
+                    null,
+                    navOptions
+                )
+            }
+            true
+        }
 
     }
 }
