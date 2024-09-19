46ce1402969a254b514c431d194e131554ed37d2
Mohammedhesham <mohammedhe4am@gmail.com>
2024-09-20 01:46:58 +0300

    Create AlertViewModel Logice And AlertViewModFactory Class

---
+++ app/src/main/java/com/example/weatherapp/alert/viewmodel/AlertViewModel.kt
@@ -0,0 +1,88 @@
+package com.example.weatherapp.alert.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.model.Alert
+import com.example.weatherapp.model.repository.WeatherRepo
+import com.example.weatherapp.network.ApiState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class AlertViewModel(private val _repo: WeatherRepo) : ViewModel() {
+
+
+    private val _alertWeather: MutableStateFlow<ApiState> = MutableStateFlow(ApiState.Loading)
+    val alertWeather = _alertWeather.asStateFlow()
+
+    private val _allAlerts: MutableStateFlow<List<Alert>> = MutableStateFlow(emptyList<Alert>())
+    val allAlerts: StateFlow<List<Alert>> = _allAlerts.asStateFlow()
+
+    private val TAG = "AlertViewModel"
+
+
+    init {
+        getAllAlerts()
+    }
+
+
+    fun getAlertWeather(
+        lat: String,
+        lon: String,
+        language: String = "",
+        units: String = ""
+    ) {
+
+        viewModelScope.launch(Dispatchers.IO) {
+            _repo.getCurrentWeatherFromRemote(lat, lon, language, units)
+                .catch { error ->
+                    _alertWeather.value = ApiState.Failure(error)
+                    Log.d(TAG, "getCurrentWeather: ${alertWeather.value}")
+                }
+                .collect { data ->
+                    _alertWeather.value = ApiState.Success(data)
+                }
+
+
+            Log.d(TAG, "getCurrentWeather: ${alertWeather.value}")
+
+        }
+
+
+    }
+
+    fun setAlertLocation(lat: String, lon: String, language: String = "", units: String = "") {
+        getAlertWeather(lat, lon, language, units)
+    }
+
+    fun getAllAlerts() {
+        viewModelScope.launch {
+            _repo.getAllAlerts().collectLatest {
+                _allAlerts.value = it
+            }
+        }
+    }
+
+    fun insertAlert(alert: Alert?) {
+
+        viewModelScope.launch {
+            _repo.insertAlert(alert)
+        }
+
+    }
+
+    fun deleteAlert(alert: Alert?) {
+
+        viewModelScope.launch {
+            _repo.deleteAlert(alert)
+        }
+
+    }
+
+
+}
