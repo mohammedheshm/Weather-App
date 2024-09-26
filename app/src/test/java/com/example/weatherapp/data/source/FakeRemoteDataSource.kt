6aa8ebab51039806b1bcea5cf1e2c5cc814cef64
Mohammedhesham <mohammedhe4am@gmail.com>
2024-09-26 17:52:46 +0300

    Handle Logic of FakeRemoteDataSource Class

--- app/src/test/java/com/example/weatherapp/data/source/FakeRemoteDataSource.kt
+++ app/src/test/java/com/example/weatherapp/data/source/FakeRemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.example.weatherapp.data.source
 
-class FakeRemoteDataSource {
+import com.example.weatherapp.model.WeatherResponse
+import com.example.weatherapp.network.WeatherRemoteDataSource
+
+class FakeRemoteDataSource(private val weatherResponse: WeatherResponse) : WeatherRemoteDataSource {
+    override suspend fun getCurrentWeather(
+        lat: String,
+        lon: String,
+        language: String,
+        units: String
+    ): WeatherResponse {
+        return weatherResponse
+    }
 }
